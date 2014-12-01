a04e4d372a8d53ed1f32403d15826dfce83ff435
IvanYanakeiv <ivannyanakiev@gmail.com>
2014-12-01 17:49:44 +0200

    Update Students.java

--- Students.java
+++ Students.java
@@ -8,80 +8,88 @@
  *
  * @author Ivan
  */
-public class Students {
+public class Student {
 
     private String name;
     private int studentID;
     private int mark1;
     private int mark2;
     private int mark3;
+ 
 
-    public Students(String name, int studentID, int mark1, int mark2, int mark3) {
-
+    public Student(int m1, int m2, int m3, int sID, String name) {
+        
         setName(name);
-        setStudentID(studentID);
-        setMark1(mark1);
-        setMark2(mark2);
-        setMark3(mark3);
+        setStudentID(sID);
+        setMark1(m1);
+        setMark2(m2);
+        setMark3(m3);
+
     }
 
-    public void setName(String vName) {
-        if (vName != null) {
-            name = vName;
-        }
+    public Student() {
+        
+        setName("");
+        setStudentID(0);
+        setMark1(0);
+        setMark2(0);
+        setMark3(0);
+
     }
 
     public String getName() {
         return name;
     }
 
-    public void setStudentID(int vStudentID) {
-        if (vStudentID > 0) {
-            studentID = vStudentID;
-        }
-    }
-
     public int getStudentID() {
         return studentID;
     }
 
-    public void setMark1(int vMark1) {
-        if (vMark1 > 0) {
-            mark1 = vMark1;
-        }
-    }
-
     public int getMark1() {
-        return mark1;
-    }
 
-    public void setMark2(int vMark2) {
-        if (vMark2 > 0) {
-            mark2 = vMark2;
-        }
+        return mark1;
     }
 
     public int getMark2() {
         return mark2;
     }
 
-    public void setMark3(int vMark3) {
-        if (vMark3 > 0) {
-            mark3 = vMark3;
-        }
-    }
-
     public int getMark3() {
         return mark3;
     }
 
-    public int getAverage() {
+    public void setName(String name0) {
+        this.name = name0;
+    }
 
-        int average;
+    public void setStudentID(int studentIDa) {
+        if (studentIDa > 0) {
+            this.studentID = studentIDa;
+        }
+    }
 
-        average = ( (mark1 + mark2 + mark3) / 3);
+    public void setMark1(int mark1a) {
+        if (mark1a >= 2 || mark1a <= 6) {
+            this.mark1 = mark1a;
+        }
+    }
 
-        return average;
+    public void setMark2(int mark2a) {
+        if (mark2a >= 2 || mark2a <= 6) {
+            this.mark2 = mark2a;
+        }
+    }
+
+    public void setMark3(int mark3a) {
+        if (mark3a >= 2 || mark3a <= 6) {
+            this.mark3 = mark3a;
+        }
+    }
+
+    public double getAverage(int i1, int i2, int i3) {
+        double result;
+        result = ((double) i1 + (double) i2 + (double) i3) / 3;
+        return result;
     }
 
 }
